4a6dfc3034374a32daefc95f692b0b95055705d2
Andreas Rønning <andreas@doomsday.no>
2018-10-18 16:32:22 +0200

    sync_get_track, sync_get_val, sync_update

--- rocket/Rocket.hx
+++ rocket/Rocket.hx
@@ -21,7 +21,22 @@ extern class Rocket {
     @:native('sync_tcp_connect')
     static function sync_tcp_connect(device:Pointer<SyncDevice>, host:String, port:UInt):Int;
 
+    @:native('sync_get_track')
+    static function sync_get_track(device:Pointer<SyncDevice>, name:String):Pointer<SyncTrack>;
+
+    @:native('sync_get_val')
+    static function sync_get_val(track:Pointer<SyncTrack>, row:Float):Float;
+
+    @:native('sync_update')
+    static function sync_update(device:Pointer<SyncDevice>, row:Int, sync_cb:Pointer<SyncCb>, cp_param:Pointer<Dynamic>):Int;
+
 } //rocket
+
+@:unreflective
+@:structAccess
+@:native("sync_cb")
+extern class SyncCb {
+}
 
 @:unreflective
 @:structAccess
@@ -33,12 +48,14 @@ extern class SyncDevice {
 	public var sync_io_cb:Dynamic;
 }
 
+@:unreflective
 @:structAccess
 @:native("track_key")
 extern class TrackKey {
 
 }
 
+@:unreflective
 @:structAccess
 @:native("sync_track")
 extern class SyncTrack {
--- test/Test.hx
+++ test/Test.hx
@@ -15,7 +15,27 @@ class Test {
             Sys.sleep(1);
         }else{
             trace("Connected");
-            Sys.sleep(6);
+            
+            var r = sync_get_track(device, "power.r");
+            var g = sync_get_track(device, "power.g");
+            var b = sync_get_track(device, "power.b");
+            var row = 0.0;
+
+            var done = false;
+            while(!done){
+                if (sync_update(device, Math.floor(row), null, null) != 0)
+			        sync_tcp_connect(device, "localhost", SYNC_DEFAULT_PORT);
+
+                var rv = sync_get_val(r, 0);
+                var gv = sync_get_val(g, 0);
+                var bv = sync_get_val(b, 0);
+                trace("State: "+rv+","+gv+","+bv);
+                if(bv==100)
+                    break;
+                Sys.sleep(0.016);
+            }
+            sync_destroy_device(device);
+            trace("Destroyed");
         }
     }
 
